c7e4d7d3fda60bae29bd39e4878c8014713e3e55
Sarah <sarahvu@mit.edu>
2018-02-16 20:52:35 -0500

    calling ColorTracker from scope instead of with tracking

--- src/extensions/scratch3_tracking/index.js
+++ src/extensions/scratch3_tracking/index.js
@@ -113,7 +113,7 @@ class Scratch3Tracking {
     }
 
     setTrackedColor(args, util){
-        var color = new tracking.ColorTracker(['magenta']); 
+        var color = new ColorTracker(['magenta']); 
         color.on('track', function(event) {
             if (event.data.length === 0) {
               // No colors were detected in this frame.
