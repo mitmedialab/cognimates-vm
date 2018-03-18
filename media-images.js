c1bf8c6eb559ac17ee4aacadfe06fb241d34011c
Stefania Druga <druga.stefania@gmail.com>
2018-03-19 00:36:26 +0100

    Build for 13889e088202b3b7e5c11aa5544e6a44b45a6631

---
+++ media-images.js
@@ -0,0 +1,8 @@
+var images = [['green flag', './playground/media/icons/event_whenflagclicked.svg'],
+			['arrow','./playground/media/icons/arrow.svg'],
+			['forever','./playground/media/icons/control_forever.svg'],
+			['repeat','./playground/media/icons/control_repeat.svg'],
+			['stop','./playground/media/icons/control_stop.svg'],
+			['wait','./playground/media/icons/control_wait.svg'],
+			['blue message','./playground/media/icons/event_broadcast_blue.svg']
+];
