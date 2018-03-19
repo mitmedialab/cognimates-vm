fa1cc38eeaf4830c998707c140116339715482ef
Stefania Druga <druga.stefania@gmail.com>
2018-03-19 06:21:52 +0100

    Revert "Fixed classifier_id error in the request to Watson"
    This reverts commit 814bbc1903334976816059fc95afe78bf88a591f.

--- src/extensions/scratch3_watson/index.js
+++ src/extensions/scratch3_watson/index.js
@@ -32,7 +32,7 @@ var visual_recognition = new VisualRecognitionV3({
 });
 
 let parameters = {
-    classifier_ids: [],
+    classifier_ids: ['default'],
     url: null,
     threshold: 0.6
   };
@@ -119,6 +119,21 @@ class Scratch3Watson {
                             defaultValue: 'Image name'
                         }
                     }                
+                }, 
+                {
+                    opcode: 'isRock',
+                    text: 'rock',
+                    blockType: BlockType.REPORTER
+                },
+                {
+                    opcode: 'isPaper',
+                    text: 'paper',
+                    blockType: BlockType.REPORTER
+                },
+                {
+                    opcode: 'isScissors',
+                    text: 'scissors',
+                    blockType: BlockType.REPORTER
                 }
                 
             ],
@@ -188,10 +203,9 @@ class Scratch3Watson {
     recognizeObject (args, util){
         var urlToRecognise = args.URL;
         parameters.url = args.URL;
-        console.log(parameters.classifier_ids[0]);
         request.get('https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify',
                     { qs : {  url: urlToRecognise,
-                            classifier_ids : parameters.classifier_ids[0],
+                            classifier_ids : parameters.classifier_ids,
                             api_key : "13d2bfc00cfe4046d3fb850533db03e939576af3", 
                             version: '2018-03-19'} 
                     },
@@ -207,8 +221,8 @@ class Scratch3Watson {
                     });
         //need to delay call to this function so the request has time to get through
         setTimeout(function(){image_class = watson_response.images[0].classifiers[0].classes[0].class;
-            console.log(image_class);}, 2500);
-            return String(image_class);
+            console.log(image_class);
+            return image_class; }, 3000);
     }
 
     getImageClass(args, util) {
@@ -222,6 +236,18 @@ class Scratch3Watson {
         });
         console.log(image_class);
         return image_class
+    }
+
+    isRock(){
+        return 'rock';
+    }
+
+    isPaper(){
+        return 'paper';
+    }
+
+    isScissors(){
+        return 'scissors';
     }
     
 }
