61637ee52490b482dfb21a2a2d7fa3ffd3db8900
Tammy Qiu <tammy.qiu@yahoo.com>
2018-03-19 19:41:57 -0400

    Added missions for Smart home and Make me happy index.js: included the missions mission8.js: mission for the simple version of make me happy smarthomecurious.js: mission for smart home - curious ver. smarthomedirectmission.js: mission for smart home - direct insturctions ver.

---
+++ src/extensions/scratch3_cognimate/missions/smarthomecurious.js
@@ -0,0 +1,111 @@
+//smart home curious mission
+let mission10 = {
+    numberSteps: ,
+    steps: [
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked"],
+            init: {
+                text: "Let's train a smart home that can understand our commands. Start off with the green flag block from events"
+            },
+            ok: {
+                text: "Alright, let's get started!"
+            },
+            bad_block: {
+                text: "Oh no! You didn't use the magic block!"
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever"],
+            init: {
+                text: "Do we want our smart home to only listen to our commands once? Or should it always be listening? What kind of control block should we use to make sure that our smart home is always waiting for our commands?"
+            },
+            ok: {
+                text: "Great job! That's exactly the right block."
+            },
+            bad_block: {
+                text: "Oops! That's not the right block try another orange block and see."
+            }   
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text"],
+            init: {
+                text: "But how do we even get our smart home to listen to what we say? What kind of block would ask a question?"
+            },
+            ok: {
+                text: "Awesome! That's the right block."
+            },
+            bad_block: {
+                text: "Not quite. Here's a hint: this is a blue block under sensing."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if"],
+            init: {
+                text: "What kind of control tests whether a statement is true or not, and then performs an action if it is? Hint, it's one of the control blocks."
+            },
+            ok: {
+                text: "You're doing great!"
+            },
+            bad_block: {
+                text: "Mmmm not this one. Try another control block."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text"],
+            init: {
+                text: "Now, inside of our if block we need some sort of operator that checks if two statements are equal or not. Where would we find a block like that?"
+            },
+            ok: {
+                text: "Great job! You found it!"
+            },
+            bad_block: {
+                "That's not the right block. Try looking under operators."
+            }
+        }
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer"],
+            init: {
+                text: "So we've already finished training our model. How can we now recognize if our command means to turn the lights on?"
+            },
+            ok: {
+                text: "Awesome! You're halfway there."
+            },
+            bad_block: {
+                text: "Not quite. We need two blocks here and then for you to fill out one of the blank spaces in the operator block."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text"],
+            init: {
+                text: "If smart home recognizes that our command means to turn the lights on. We should use a block that will turn a light on, right? Try looking for one under the Hue extension."
+            },
+            ok: {
+                text: "Great job!"
+            },
+            bad_block: {
+                text: "That's not the right block. Try looking for another one under the extension."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text"],
+            init: {
+                text: "Now do you think you can do what you did again, but this time turning the lights off? Give it a try!"
+            },
+            ok: {
+                text: "Fantastic job! You've just successfully programmed your own smart home!"
+            },
+            bad_block: {
+                text: "That's not the right block. Make sure you follow the same steps that we did for turning the lights on. But make sure to set everything as lights off!"
+            }
+        }
+    ]
+};
+module.exports = mission10;
---
+++ src/extensions/scratch3_cognimate/missions/smarthomedirectmission.js
@@ -0,0 +1,111 @@
+//smart home mission (with instructions)
+let smarthomedirectmission = {
+    numberSteps: 8,
+    steps:[
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked"],
+            init: {
+                text: "Let's train a smart home that can understand our commands. Start off with the green flag block from events"
+            },
+            ok: {
+                text: "Alright, let's get started!"
+            },
+            bad_block: {
+                text: "Oh no! You didn't use the magic block!"
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever"],
+            init: {
+                text: "We always want our lights to be waiting for our commands. So let's make sure to wrap everything with a forever loop under controls."
+            },
+            ok: {
+                text: "Great job!"
+            },
+            bad_block: {
+                text: "Oh no! Look for the block that says forever in controls."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text"],
+            init: {
+                text: "Now write your smart home a message and save it in an answer variable. You should use the Ask and Wait block under sensing."
+            },
+            ok: {
+                text: "Awesome!"
+            },
+            bad_block: {
+                text: "Oh no! You didn't use the right block."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if"],
+            init: {
+                text: "The smart lights should turn on depending on what your command is. Let's start by placing using an empty if block."
+            },
+            ok: {
+                text: "Great job!"
+            },
+            bad_block: {
+                text: "Oh no! That's not the right one."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text"],
+            init: {
+                text: "Now we want to see if the smart lights think our message means lights on. For that, put a green equals block into the if block."
+            },
+            ok: {
+                text: "Awesome! Let's move on to the next step."
+            },
+            bad_block: {
+                text: "Oh no! Make sure you're using the gren equals block under operators."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer"],
+            init: {
+                text: "Using your trained Watson model, you want to see if the smart lights recognize your command as lights on. You can find the recognize text block under the Watson extension."
+            },
+            ok: {
+                text: "Great job! Let's move on."
+            },
+            bad_block: {
+                text: "Those aren't the right blocks. Make sure you're using your answer variable, the Watson recognize text block, and filling in the other side of the equals operator."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text"],
+            init: {
+                text: "If your model recognizes your command to mean lights on, we can have the smart lights turn on. Nest the Set light status to On block under the if block."
+            },
+            ok: {
+                text: "Great job! You're almost done."
+            },
+            bad_block: {
+                text: "That's not the right block. Try again."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text"],
+            init: {
+                text: "Now let's repeat what we did above to turn the lights on on the opposite command. Turning the light off."
+            },
+            ok: {
+                text: "Fantastic job! You've just successfully programmed your own smart home!"
+            },
+            bad_block: {
+                text: "That's not the right block. Make sure you follow the same steps that we did for turning the lights on. But make sure to set everything as lights off!"
+            }
+        }
+    ]
+};
+module.exports = mission9;
