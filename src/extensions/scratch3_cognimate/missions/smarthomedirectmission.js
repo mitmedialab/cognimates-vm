6b5c92446cd8947cfccf7b5518367a175cbffd95
Tammy Qiu <tammy.qiu@yahoo.com>
2018-03-22 00:18:29 -0400

    Fixed step increments in smart home direct mission

--- src/extensions/scratch3_cognimate/missions/smarthomedirectmission.js
+++ src/extensions/scratch3_cognimate/missions/smarthomedirectmission.js
@@ -1,6 +1,6 @@
 //smart home mission (with instructions)
 let mission9 = {
-    numberSteps: 8,
+    numberSteps: 13,
     steps:[
         {
             init_blocks: ["tutor.mission", "tutor.menu.mission"],
@@ -74,10 +74,10 @@ let mission9 = {
         },
         {
             init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text"],
-            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext"],
             init: {
                 text: "Using your trained Watson model, you want to see if the smart lights recognize your command as lights on. \
-                You can find the recognize text block under the Watson extension."
+                You can find the recognize text block under the Watson extension and put it in one of the blanks."
             },
             ok: {
                 text: "Great job! Let's move on."
@@ -85,6 +85,19 @@ let mission9 = {
             bad_block: {
                 text: "Those aren't the right blocks. Make sure you're using your answer variable, the Watson recognize text \
                 block, and filling in the other side of the equals operator."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer"],
+            init: {
+                text: "In the other blank put in your answer variable so it can be compared to what Watson thinks the answer means."
+            },
+            ok: {
+                text: "Awesome!"
+            },
+            bad_block: {
+                text: "Oops! try again."
             }
         },
         {
@@ -103,18 +116,70 @@ let mission9 = {
         },
         {
             init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text"],
-            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text", "control_if"],
             init: {
-                text: "Now let's repeat what we did above to turn the lights on on the opposite command. Turning the light off."
+                text: "Now let's repeat what we did above to turn the lights on on the opposite command. Turning the light off. Start with another if statement."
             },
             ok: {
-                text: "Fantastic job! You've just successfully programmed your own smart home!"
+                text: "Fantastic!"
             },
             bad_block: {
-                text: "That's not the right block. Make sure you follow the same steps that we did for turning the lights on.\
-                 But make sure to set everything as lights off!"
+                text: "That's not the right block."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text", "control_if"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text", "control_if", "operator_equals","text","text"],
+            init: {
+                text: "Let's put another equals operator into the if block."
+            },
+            ok: {
+                text: "Good job!"
+            },
+            bad_block: {
+                text: "Oops! Try again."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text", "control_if", "operator_equals","text","text"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text", "control_if", "operator_equals","text","text", "watson_recognizetext"],
+            init: {
+                text: "You know the drill, put the watson recognize text into one of the blanks"
+            },
+            ok: {
+                text: "Let's move on."
+            },
+            bad_block: {
+                text: "Oh no! You're not using the right block."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text", "control_if", "operator_equals","text","text", "watson_recognizetext"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text", "control_if", "operator_equals","text","text", "watson_recognizetext"],
+            init: {
+                text: "Put the answer variable inot the other blank."
+            },
+            ok: {
+                text: "Great! We're almost there!"
+            },
+            bad_block: {
+                text: "Oops! Try again."
+            }
+        },
+        {
+            init_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text", "control_if", "operator_equals","text","text", "watson_recognizetext"],
+            end_blocks: ["tutor.mission", "tutor.menu.mission", "event_whenflagclicked", "control_forever", "sensing_askandwait", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "sensing_answer", "hue_setLightStatus", "text", "text", "control_if", "operator_equals","text","text", "watson_recognizetext", "hue_setLightStatus", "text", "text"],
+            init: {
+                text: "Finally, nest the set lights status to off block in the if block."
+            },
+            ok: {
+                text: "You've completed your mission! Good job!"
+            },
+            bad_block: {
+                text: "Mmm not quite, try again."
             }
         }
     ]
 };
+
 module.exports = mission9;
