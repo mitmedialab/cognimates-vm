a55c042fa91c70be7641becbadce6d7ad6ef3602
satvu <sarahvu@mit.edu>
2018-03-29 22:45:24 -0400

    classifier_id as string and add default to model menu

--- src/extensions/scratch3_watson/index.js
+++ src/extensions/scratch3_watson/index.js
@@ -9,26 +9,17 @@ const RenderedTarget = require('../../sprites/rendered-target');
 
 const iconURI = require('./assets/watson_icon');
 
-// camera to be added later
-
 //variable to make sure requests are complete before continuing
 let requestInProgress = false;
 
 //models and their classifier_ids
 const modelDictionary = {
-    RockPaperScissors: 'RockPaperScissors_371532596'
+    RockPaperScissors: 'RockPaperScissors_371532596',
+    Default: 'default'
 };
 
 // watson
 var watson = require('watson-developer-cloud');
-//watson assistant/conversation
-var AssistantV1 = require('watson-developer-cloud/conversation/v1');
-var assistant = new AssistantV1({
-    version_date: '2018-02-16',
-    username: "0a425f9f-919a-422c-bac7-b9ce3de71949",
-    password: "xkCnqszwIFvF",
-    url: 'https://gateway-fra.watsonplatform.net/assistant/api'
-});
 //watson visual recognition
 var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
 var visual_recognition = new VisualRecognitionV3({
@@ -36,25 +27,13 @@ var visual_recognition = new VisualRecognitionV3({
   api_key: '13d2bfc00cfe4046d3fb850533db03e939576af3',
   version_date: '2016-05-20'
 });
-let parameters = {
-    classifier_ids: ['default'],
-    url: null,
-    threshold: 0.6 
-  };  
-var params = {
-    //images_file: null,
-    parameters: parameters
-};
+//classifier_id
+let classifier_id = 'default'
 
 //for parsing image response
 let watson_response; //the full response
 let classes = {}; //the classes and scores returned for the watson_response
 let image_class; //the highest scoring class returned for an image
-
-//for parsing assistant response
-let assistant_response; 
-let labels = {};
-let text_label;
 
 class Scratch3Watson {
     constructor (runtime) {
@@ -113,55 +92,31 @@ class Scratch3Watson {
                             defaultValue: 'label name'
                         }
                     }
-                },
-                {
-                    opcode: 'recognizeText', 
-                    blockType: BlockType.REPORTER,
-                    text: 'recognize text [TEXT] label',
-                    arguments:{
-                        TEXT: {
-                            type: ArgumentType.STRING,
-                            defaultValue: 'type a message!'
-                        }
-                    }
-                },
-                {
-                    opcode: 'getTextScore', 
-                    blockType: BlockType.REPORTER,
-                    text: 'score for text label [CLASS]',
-                    arguments:{
-                        CLASS: {
-                            type: ArgumentType.STRING,
-                            defaultValue: 'class name'
-                        }
-                    }
                 }     
             ],
             menus: {
-                models: ['RockPaperScissors']
+                models: ['RockPaperScissors', 'Default']
             }
         };
     }
 
 
     getModelFromList (args, util){
-        parameters.classifier_ids[0] = modelDictionary[args.MODELNAME];
+        classifier_id = modelDictionary[args.MODELNAME];
     }
 
     getModelfromString (args, util){
-        parameters.classifier_ids[0] = args.IDSTRING;
+        classifier_id = args.IDSTRING;
     }
     
     recognizeObject (args, util){
         if (requestInProgress == true) { // Stop if you're still waiting for request to finish
             util.yield(); // Stop Scratch from executing the next block
         } else{
-        var urlToRecognise = args.URL;
-            parameters.url = args.URL;
-            console.log(parameters.classifier_ids[0]);
+            var urlToRecognise = args.URL;
             request.get('https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify',
                         { qs : {  url: urlToRecognise, threshold: 0.0,
-                                classifier_ids : parameters.classifier_ids[0],
+                                classifier_ids : classifier_id,
                                 api_key : '13d2bfc00cfe4046d3fb850533db03e939576af3',
                                 version: '2018-03-19'} 
                         },
@@ -219,51 +174,6 @@ class Scratch3Watson {
         console.log(classes);
         console.log(classes[comparison_class]);
         return classes[comparison_class];
-    }
-
-    recognizeText(args, util){
-        var message = args.TEXT;
-        if (requestInProgress == true) { // Stop if you're still waiting for request to finish
-            util.yield(); // Stop Scratch from executing the next block
-        } else{
-            /*
-            assistant.message({
-                workspace_id: "7d9b43b7-0f5b-4ab2-8979-7ad1c1891221",
-                input: {'text': 'Hello'}
-            },  function(err, response) {
-                if (err)
-                console.log('error:', err);
-                else
-                console.log(JSON.stringify(response, null, 2));
-            }); */
-            request.get('https://gateway-fra.watsonplatform.net/assistant/api/v1/message',
-                { qs : { input: {text: message}, workspace_id: "7d9b43b7-0f5b-4ab2-8979-7ad1c1891221",
-                        username: "0a425f9f-919a-422c-bac7-b9ce3de71949",
-                        password: "xkCnqszwIFvF" } 
-                },
-                function (err, response) {
-                    if (err){
-                        console.log(err);
-                    }
-                    else{
-                        console.log(JSON.stringify(response, null, 2));
-                        assistant_response = JSON.parse(JSON.stringify(response, null, 2));
-                        assistant_response = JSON.parse(assistant_response.body);
-                    }
-                });
-            if(assistant_response === null){
-                requestInProgress = true; //set status to waiting
-                util.yield(); //block execution of next block   
-            }
-            if(assistant_response !== null){
-                    return image_class;
-             }
-
-        }   
-    }
-    
-    getTextScore(args, util){
-        return null;
     }
     
 }
