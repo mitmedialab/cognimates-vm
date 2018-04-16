352b889c878e63fbc0ee338909385374c5ad82ff
satvu <sarahvu@mit.edu>
2018-04-16 17:25:58 -0400

    http testing of watson_nlc_proxy server for update classifier block

--- src/extensions/scratch3_watson/index.js
+++ src/extensions/scratch3_watson/index.js
@@ -26,19 +26,22 @@ const modelDictionary = {
 };
 
 //server info
-let apiURL = 'http://gateway-a.watsonplatform.net/visual-recognition/api';
+let apiURL = 'https://gateway-a.watsonplatform.net/visual-recognition/api';
 //let classifyURL = 'http://cognimate.me:3477/visual/classify';
-let classifyURL = 'http://localhost:3477/visual/update'
+let classifyURL = 'http://localhost:3477/visual/classify';
+let updateURL = 'http://localhost:3477/visual/update';
 
 //classifier_id
 let classifier_id = 'default';
 let api_key = "1438a8fdb764f1c8af8ada02e6c601cec369fc40";
-//let api_key = '13d2bfc00cfe4046d3fb850533db03e939576af3';
 
 //for parsing image response
 let watson_response; //the full response
 let classes; //the classes and scores returned for the watson_response
 let image_class; //the highest scoring class returned for an image
+
+//response when updating a classifier
+let update_response;
 
 //image that user takes
 let videoElement;
@@ -369,8 +372,8 @@ class Scratch3Watson {
                     callback(error, body);
                 });
         }
-
     }
+
 
     setAPI(args, util){
         if(args.STRING === 'key')
@@ -400,13 +403,19 @@ class Scratch3Watson {
     updateClassifier(args, util){
         if(imageData.substring(0,4) === 'data'){
             request.post({
-                url:     classifyURL,
+                url:     updateURL,
                 form:    { api_key: "1438a8fdb764f1c8af8ada02e6c601cec369fc40", 
                             version_date: '2018-03-19', classifier_id: classifier_id,
                             label: args.LABEL,
-                            positive_examples: imageData }
-                }, function(error, response, body){
-                    callback(error, body);
+                            positive_example: imageData }
+                }, function(err, response, body) {
+                    if (err)
+                        console.log(err);
+                    else {
+                        update_response = response.body;
+                        console.log(response);
+                        console.log(update_response);
+                    }
                 });
         } else{
             return 'Only use webcam photos!'
