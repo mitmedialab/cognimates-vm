8f474df0b71f4c8a33737cf1398ce9c258004720
Stefania Druga <druga.stefania@gmail.com>
2018-04-18 11:33:52 -0400

    change language vision

--- src/extensions/scratch3_watson/index.js
+++ src/extensions/scratch3_watson/index.js
@@ -194,19 +194,19 @@ class Scratch3Watson {
                     arguments: {
                         IDSTRING: {
                             type: ArgumentType.STRING,
-                            defaultValue: 'classifier id'
+                            defaultValue: 'model id'
                         }
                     }
                 },
                 {
                     opcode: 'takePhoto',
                     blockType: BlockType.COMMAND,
-                    text: 'take photo from webcam'
+                    text: 'Take photo from webcam'
                 },
                 {
                     opcode: 'setPhotoFromURL',
                     blockType: BlockType.COMMAND,
-                    text: 'take photo from url [URL]',
+                    text: 'Use photo from url [URL]',
                     arguments: {
                         URL: {
                             type: ArgumentType.STRING,
@@ -217,38 +217,38 @@ class Scratch3Watson {
                 {
                     opcode: 'recognizeObject', 
                     blockType: BlockType.REPORTER,
-                    text: 'get the label for photo',
+                    text: 'What do you see in the photo?',
                 },
                 {
                     opcode: 'getScore', 
                     blockType: BlockType.REPORTER,
-                    text: 'score for image label [CLASS]',
+                    text: 'How sure are you the photo is a [CLASS]?',
                     arguments:{
                         CLASS: {
                             type: ArgumentType.STRING,
-                            defaultValue: 'label name'
+                            defaultValue: 'add category here'
                         }
                     }
                 },
                 {
                     opcode: 'clearResults',
                     blockType: BlockType.COMMAND,
-                    text: 'clear results'
+                    text: 'Clear results'
                 },
                 {
                     opcode: 'updateClassifier',
                     blockType: BlockType.COMMAND,
-                    text: 'update classifier label [LABEL] with photo',
+                    text: 'Add photo to [LABEL]',
                     arguments:{
                         LABEL:{
                             type: ArgumentType.STRING, 
-                            defaultValue: 'label name'
+                            defaultValue: 'add category here'
                         }
                     }
                 }
             ],
             menus: {
-                models: ['RockPaperScissors', 'Default']
+                models: ['Default','RockPaperScissors']
             }
         };
     }
