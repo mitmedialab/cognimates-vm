17f4338b782367d7e163a214628c330b4db2eb17
Tammy Qiu <tammy.qiu@yahoo.com>
2018-05-05 21:26:45 -0400

    changed sounds for speech recognition and changed name of speech extension

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -49,7 +49,7 @@ const builtinExtensions = {
     tracking:Scratch3TrackingBlocks,
     watson:Scratch3WatsonBlocks,
     watson_nlc:Scratch3WatsonNLCBlocks,
-    watson_speech:Scratch3SpeechBlocks,
+    speech:Scratch3SpeechBlocks,
     wemo: Scratch3WemoBlocks
 };
 
