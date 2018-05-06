9b2359e22316ec9e56ffa55810a5c616d5292de1
satvu <sarahvu@mit.edu>
2018-05-05 22:37:02 -0400

    extension working with a server

--- src/extensions/scratch3_twitter/index.js
+++ src/extensions/scratch3_twitter/index.js
@@ -7,15 +7,8 @@ const request = require('request');
 const RenderedTarget = require('../../sprites/rendered-target');
 const ajax = require('es-ajax');
 const iconURI = require('./assets/twitter_icon');
-const dotenv = require('dotenv').config()
-var Twitter = require('twitter');
 
-var client = new Twitter({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-  });
+let server_url = 'http://localhost:3477/twitter/call';
 
 class Scratch3Twitter {
     constructor (runtime) {
@@ -67,35 +60,35 @@ class Scratch3Twitter {
     latestUserTweet(args, util) {
         var user = args.USER;
         var params = {screen_name: user, count:1};
-        /*
-        client.get('statuses/user_timeline.json', params, 
-            function(error, tweet, response){
-                if(err){console.logg(err)}
-                console.log(tweet);
-        });*/
-        request.get({url:'https://cors-anywhere.herokuapp.com/https://api.twitter.com/1.1/search/tweets.json?q=nasa&result_type=popular', 
-                    headers: {Authorization: {Oauth: {}}},
-                    function(err, response){
-                        if (err){
-                            console.log(err);
-                        }
-                        else {
-                            console.log(response)
-                        }
-                    }});
+        var uri = 'statuses/user_timeline.json';
+        request.post({
+            url: server_url,
+            form: { uri: uri, params: params}
+            }, function(err, response){
+                if(err){console.log(err)}
+                else{
+                    console.log(response);
+                    return response;
+                }
+            });
     }
+
 
     getTopTweet(args, util){
         var category = args.CATEGORY;
         var hashtag = encodeURIComponent(args.HASH);
-        var params = {q: hashtag, result_type: category, count: 1}
-        request.get("/search/tweets", params,
-            function(err, tweet, response){
-                if (err){
-                    console.log(err);
+        var params = {q: hashtag, result_type: category, count: 1};
+        var uri = '/search/tweets';
+        request.post({
+            url: server_url,
+            form: { uri: uri, params: params}
+            }, function(err, response){
+                if(err){console.log(err)}
+                else{
+                    console.log(response);
+                    return response;
                 }
-                console.log(tweet);
-        });
+            });
     }
 
 }
