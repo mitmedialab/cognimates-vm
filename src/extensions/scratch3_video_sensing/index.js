dc64cd2b7f4237e9676483b4de9f0b69d0f690bf
satvu <sarahvu@mit.edu>
2018-06-11 11:59:55 +0800

    get array of devices

--- src/extensions/scratch3_video_sensing/index.js
+++ src/extensions/scratch3_video_sensing/index.js
@@ -10,7 +10,7 @@ const Video = require('../../io/video');
 const VideoMotion = require('./library');
 
 let devicePromise = navigator.mediaDevices.enumerateDevices();
-let allDevices;
+let allDevices = [];
 let videoSources = {};
 let current_source;
 /**
@@ -69,6 +69,7 @@ class Scratch3VideoSensingBlocks {
         this.runtime = runtime;
 
         this._organizeDevices();
+        console.log(allDevices, videoSources);
 
         /**
          * The motion detection algoritm used to power the motion amount and
@@ -420,8 +421,8 @@ class Scratch3VideoSensingBlocks {
                 ATTRIBUTE: this._buildMenu(this.ATTRIBUTE_INFO),
                 SUBJECT: this._buildMenu(this.SUBJECT_INFO),
                 VIDEO_STATE: this._buildMenu(this.VIDEO_STATE_INFO),
-                // VIDEO_SOURCE: ['Default Camera', 'USB camera', 'Cozmo', 'Jibo']
-                VIDEO_SOURCE: this._buildMenu(this.videoSources)
+                VIDEO_SOURCE: ['1', '2', '3', '4']
+                // VIDEO_SOURCE: this._buildMenu(this.videoSources)
             }
         };
     }
@@ -511,14 +512,17 @@ class Scratch3VideoSensingBlocks {
      * Set the provider to the videoSource Object
      */
     setVideoSource(args, util){
-       return;
+        return;
     }
 
     _organizeDevices(){
-        devicePromise.then(allDevices => allDevices);
-        allDevices.forEach((device) => {
-            if(device.kind === 'videoinput'){
-                videoSources[device.label] = device;
+        devicePromise.then(function(deviceInfos){
+            for (var i = 0; i !== deviceInfos.length; ++i) {
+                var deviceInfo = deviceInfos[i];
+                if (deviceInfo.kind === 'videoinput') {
+                   videoSources[i+1] = deviceInfo;
+                   allDevices.push(deviceInfo.label);
+                }
             }
         });
     }
